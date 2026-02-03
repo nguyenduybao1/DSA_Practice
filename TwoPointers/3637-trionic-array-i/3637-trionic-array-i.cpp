fe3f281d9837d2a5d634891e6bece2549265b24f
nguyenduybao1 <nguyenbao7795@gmail.com>
2026-02-03 21:13:26 +0700

    Add Trionic Array I solution (TwoPointers)

---
+++ TwoPointers/3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -0,0 +1,33 @@
+/*
+Leetcode 3637 - Trionic Array I
+Pattern:  Linear Scan(Monotonic Segments)
+
+Idea:
+- Scan the array in three monotonic phases:
+  increasing -> decreasing -> increasing.
+- Each phase must be non-empty.
+- If we reach the end exactly after the third phase, return true.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 4) return false;
+
+        int i = 0;
+        while (i < n - 1 && nums[i] < nums[i+1]) i++;
+        if (i == 0) return false;
+
+        int j = i;
+        while (j < n - 1 && nums[j] > nums[j+1]) j++;
+        if (j == i || j == n - 1) return false;
+
+        int k = j;
+        while (k < n - 1 && nums[k] < nums[k+1]) k++;
+
+        return k == n - 1;
+    }
+};
